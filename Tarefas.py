6a0f24a316a33b896eb55bd93207636c8b63b67a
gabrielvf1 <vf_gabriel@yahoo.com.br>
2019-11-19 11:28:06 -0300

    Update Tarefas.py

--- Tarefas.py
+++ Tarefas.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_restful import Api, Resource, reqparse
+import sys
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,46 +10,55 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('Tarefa', type=str)
 
-Tarefas = {1: "Dormir", 2: "Andar"}
-a = 3
+f= open("/tmp/dados.txt","r")
+dados = f.readlines()
+for i in dados:
+    linha = i.split()
+    if linha[0] == 'ipPublic:':
+        PublicIP = linha[1]
 
-
+# PublicIP = '18.188.94.107'
 class Tarefa(Resource):
     def get(self):
-        return jsonify(Tarefas)
+        tarefas = requests.get('http://' + PublicIP + ':8080' + '/Tarefa/')
+        return tarefas.json()
 
     def post(self):
         request.get_json(force=True)
         args = parser.parse_args()
         tarefa =  str(args['Tarefa'])
-        # tarefa = request.args.get("Tarefa")
-        Tarefas[len(Tarefas)+1] = tarefa
-        return jsonify({"Tarefas": Tarefas})
+        resposta = requests.post('http://' + PublicIP + ':8080' + '/Tarefa/', json={'Tarefa': tarefa})
+        return resposta.json()
 
 
 class Nada(Resource):
     def get(self):
-        return "Hello WOrld"
+        tarefas = requests.get('http://' + PublicIP + ':8080' + '/')
+        return tarefas.json()
 
 
 class TarefaId(Resource):
     def get(self, id):
-        return jsonify({"Tarefa": Tarefas[id]})
+        tarefas = requests.get('http://' + PublicIP + ':8080' + '/Tarefa/' + str(id))
+        return tarefas.json()
 
     def put(self, id):
         request.get_json(force=True)
         args = parser.parse_args()
         tarefa =  str(args['Tarefa'])
-        Tarefas[id] = tarefa
-        return jsonify({"Tarefas": Tarefas})
+        print('http://' + PublicIP + ':8080' + '/Tarefa/' + str(id))
+        a = requests.put('http://' + PublicIP + ':8080' + '/Tarefa/' + str(id), json={'Tarefa': tarefa})
+        return a.json()
 
     def delete(self, id):
         del Tarefas[id]
-        return jsonify({"Tarefas": Tarefas})
+        resposta = requests.delete('http://' + PublicIP + ':8080' + '/Tarefa/' + str(id))
+        return resposta.json()
 
 class HealthCheck(Resource):
     def get(self):
-        return  jsonify({"Status": 200})
+        tarefas = requests.get('http://' + PublicIP + ':8080' + '/HealthCheck')
+        return  tarefas.json()
 
 api.add_resource(Tarefa, '/Tarefa/')
 api.add_resource(HealthCheck, '/HealthCheck')
@@ -57,4 +68,3 @@ api.add_resource(Nada, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0', port=8080)
-
